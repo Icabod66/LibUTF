c2bc00ca0e1e1d5b27d1117e3d2ce87765db4dcd
Ritchie Brannan <ritchie@morphicvoid.com>
2026-03-28 19:10:33 +0000

    Build fixes
    Fixed references and cleaned up the build paths,

---
+++ src/main/suite_utf.cpp
@@ -0,0 +1,21 @@
+
+//  SuiteUTF
+//  Original design 2010–2016; maintained and extended 2024–2025.
+//  Copyright (c) 2010–2025 Ritchie Brannan.
+//  MIT License. See LICENSE.txt. Project history: docs/History.md.
+//
+//  File:   suite_utf.cpp
+//  Author: Ritchie Brannan
+//  Date:   14 Dec 25
+//  
+//  Description:
+//  
+//  	Starting point for self-contained SuiteUTF applications.
+
+#include <iostream>
+#include "suite_utf.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
